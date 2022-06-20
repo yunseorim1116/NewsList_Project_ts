08f1ea22dfa6e79521754ee32cc26072cca7d96f
seorim <hhyjs2r@gmail.com>
2022-06-20 20:27:22 +0900

    타이틀,인풋 디자인

--- src/component/news/News.tsx
+++ src/component/news/News.tsx
@@ -1,18 +1,22 @@
 import React from "react";
-import Content from "./GetContentData";
-import Search from "./Search";
+import GetContentData from "./GetContentData";
+
+import styled from "styled-components";
 // https://api.nytimes.com/svc/archive/v1/2019/1.json?api-key=yourkey
 const News = () => {
-
-  
-
   return (
     <div>
-      <h1>News</h1>
+      <HeaderDiv>The New York Times</HeaderDiv>
+      <br />
 
-      <Content />
+      <GetContentData />
     </div>
   );
 };
+
+const HeaderDiv = styled.div`
+  text-align: center;
+  font-size: 28px;
+`;
 
 export default News;
--- src/component/news/Search.tsx
+++ src/component/news/Search.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, MouseEventHandler, useRef } from "react";
 import axios from "axios";
 import Histroy from "./Histroy";
+import styled from "styled-components";
 
 interface Iprops {
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -23,18 +24,42 @@ const Search = ({ onChange, setHistory }: Iprops) => {
     console.log("섭밋!");
   };
   return (
-    <>
+    <FlexDivBox>
       <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          onFocus={onHandleFocus}
-          onBlur={onHandleBlur}
-        />
-        <button type="submit">검색</button>
+        <InputSearchDiv>
+          <InputSearch
+            onChange={onChange}
+            onFocus={onHandleFocus}
+            onBlur={onHandleBlur}
+          />
+        </InputSearchDiv>
+        {/* <button type="submit">검색</button> */}
       </form>
       {focus ? <Histroy setHistory={setHistory} /> : ""}
-    </>
+    </FlexDivBox>
   );
 };
+
+const FlexDivBox = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+const InputSearchDiv = styled.div`
+  background: #fff;
+  border: none;
+  border-radius: 22px;
+  box-shadow: 0 1px 6px 0 rgb(32 33 36 / 28%);
+
+  max-width: 560px;
+  outline: none;
+`;
+const InputSearch = styled.input`
+  background: none;
+  border: none;
+  height: 44px;
+  outline: none;
+  padding: 0 40px 2px;
+  width: 100%;
+`;
 
 export default Search;
